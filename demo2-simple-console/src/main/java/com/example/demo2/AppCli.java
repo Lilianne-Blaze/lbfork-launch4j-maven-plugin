59b8904253e4b49179c8dd29c495e176b37729e2
Lilianne-Blaze <39486911+Lilianne-Blaze@users.noreply.github.com>
2024-01-06 02:33:47 +0100

    Added demo1, demo2

---
+++ demo2-simple-console/src/main/java/com/example/demo2/AppCli.java
@@ -0,0 +1,14 @@
+package com.example.demo2;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class AppCli {
+
+    public static void main(String[] args) throws Exception {
+	log.info("Hello world!");
+	log.info("Sleeping for 3 min...");
+	Thread.sleep(1000 * 60 * 3);
+    }
+
+}
